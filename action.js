c92f806d0f51eb148e5add5c2c094575fa7f0391
Sonia <sohoshopaholic@gmail.com>
2017-04-21 19:56:22 -0400

    made the volunteer page moble responsive

--- action.js
+++ action.js
@@ -3,24 +3,42 @@
 
 /*EMAIL*/
 
-function email () {
+function email() {
+	var st1 = '<a ';
+	var st2 = 'hre';
+	var st3 = 'f="mai';
+	var st4 ='lto:';
 
-var st1 = "<a ";
-var st2 = "hre";
-var st3 = "f=mai";
-var st4 ="lto:";
+	var e1 = 'info';
+	var e2 = '@';
+	var e3 = 'evessanctuary';
+	var e4 = '.';
+	var e5 = 'com'
+	var eDone = e1+e2+e3+e4+e5;
 
-var e1 = "info";
-var e2 = "@";
-var e3 = "evessanctuary";
-var e4 = ".";
-var e5 = "com"
-var eDone = e1+e2+e3+e4+e5;
+	var end1 = '</';
+	var end2 = 'a>';
 
-var end1 = "</";
-var end2 = "a>";
+	document.write(st1+st2+st3+st4+eDone+'">'+eDone+end1+end2);
+}
 
-document.write(st1+st2+st3+st4+eDone+">"+eDone+end1+end2);
+function emailToBuy() {
+	var st1 = '<a class="buy-button" ';
+	var st2 = 'hre';
+	var st3 = 'f="mai';
+	var st4 ='lto:';
+
+	var e1 = 'info';
+	var e2 = '@';
+	var e3 = 'evessanctuary';
+	var e4 = '.';
+	var e5 = 'com'
+	var eDone = e1+e2+e3+e4+e5;
+
+	var end1 = '</';
+	var end2 = 'a>';
+
+	document.write(st1+st2+st3+st4+eDone+'">'+'Email to Buy Poochee Harness'+end1+end2);
 }
 
 /* If I wanted  to put a subject in I would add:  
